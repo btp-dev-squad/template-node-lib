d68a2bbe6c8d25c5ea98582b729c23bc731eca5c
Arun Krishnamoorthy <arun.krishnamoorthy@aarini.com>
2025-09-21 08:56:29 +0200

    fix: Prevent branch deletion on pull request

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -209,7 +209,7 @@ repo_settings_payload=$(cat <<EOF
   "allow_squash_merge": true,
   "allow_merge_commit": false,
   "allow_rebase_merge": false,
-  "delete_branch_on_merge": true,
+  "delete_branch_on_merge": false,
   "allow_auto_merge": false
 }
 EOF
